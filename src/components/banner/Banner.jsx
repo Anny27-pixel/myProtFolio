47c190b9f64d8a13c45375cd6aabf4ac4678c74b
Anny27-pixel <jabintasmin102523@gmail.com>
2025-01-22 23:35:43 +0600

    start banner or home page design

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,8 +1,21 @@
 
 const Banner = () => {
   return (
-    <section id="home" className="w-full h-[800px] pt-10 pb-20 flex border-b-[1px] border-b-slate-800">
-        Banner
+    <section id="home" className="w-full h-[800px] pt-10 pb-20 flex border-b-[2px] border-b-slate-800 font-titleFont">
+        <div className="w-1/2" >
+            <div className="flex flex-col gap-6">
+                <h4 className="text-lg font-normal">Hello Everyone , Welcome to my Era</h4>
+                <h1 className="text-4xl font-bold text-slate-300">
+                    Hi, I am 
+                    <span className="text-colorfulText text-6xl"> JABIN TASMIN ANNY !!!</span>
+                </h1>
+                <h2 className="text-2xl font-bold text-slate-300">
+                    Hire me as a <span>Mern Stack Developer</span>
+                </h2>
+                <p className="text-base font-bodyFont leading-6 tracking-wide">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem perspiciatis a iusto corrupti quia est molestias veritatis commodi sit. Sit sunt corrupti velit consequuntur repellat fuga voluptatum placeat accusantium illo.</p>
+            </div>
+        </div>
+        <div className="w-1/2" >hhh</div>
     </section>
   );
 }
